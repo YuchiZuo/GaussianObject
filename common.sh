f44d6828311f1afbe55bb69d35b2d09b129c430d
真学不会 <03211003@cumt.edu.cn>
2024-12-30 10:18:44 +0800

    requires_grad_

--- common.sh
+++ common.sh
@@ -1,5 +1,13 @@
 ## g_o
-python train_gs.py -s data/R63_two_view -m output/gs_init/my_car -r 4 --sparse_view_num 2 --sh_degree 2 --init_pcd_name Point3D --white_background --random_background
+python train_gs.py \
+    -s data/R63_two_view \
+    -m output/gs_init/my_car \
+    -r 4 \
+    --sparse_view_num 2 \
+    --sh_degree 2 \
+    --init_pcd_name Point3D \
+    --save_iterations 1 50 100 \
+    --random_background
 
 python render.py \
     -m output/gs_init/my_car \
@@ -10,5 +18,7 @@ python render.py \
 python render.py \
     -m output/gs_init/my_car \
     --sparse_view_num 2 --sh_degree 2 \
+    --iteration 1 \
+    --render_resize_method pad \
     --init_pcd_name Point3D \
     --white_background --render_path
